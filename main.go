5b1b4d1926c6a96764e4ee21eeac4b6d7a6d0019
gaponukz <gaponukz54@gmail.com>
2023-05-30 00:52:40 +0300

    Add RequiredMethod decoracor

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ import (
 	"./src/storage"
 )
 
+type RouterFunc = func(rw http.ResponseWriter, r *http.Request)
+
+func RequiredMethod(router RouterFunc, required string) RouterFunc {
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		if request.Method == required {
+			router(responseWriter, request)
+
+		} else {
+			http.Error(responseWriter, "Method Not Allowed", http.StatusMethodNotAllowed)
+		}
+	}
+}
+
 func main() {
 	routerService := controller.Controller{
 		Storage:  &storage.UserMemoryStorage{},
@@ -20,9 +33,9 @@ func main() {
 
 	httpRoute := http.NewServeMux()
 
-	httpRoute.HandleFunc("/rate", routerService.RateRouter)
-	httpRoute.HandleFunc("/subscribe", routerService.SubscribeRouter)
-	httpRoute.HandleFunc("/sendEmails", routerService.SendEmailsRouter)
+	httpRoute.HandleFunc("/rate", RequiredMethod(routerService.RateRouter, http.MethodGet))
+	httpRoute.HandleFunc("/subscribe", RequiredMethod(routerService.SubscribeRouter, http.MethodPost))
+	httpRoute.HandleFunc("/sendEmails", RequiredMethod(routerService.SendEmailsRouter, http.MethodPost))
 
 	server := http.Server{
 		Addr:    ":8080",
